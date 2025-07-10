de24aff56c43917a8988cb1fb27f6274f0ef8d2e
pigglehyun <sarah0661@pusan.ac.kr>
2025-07-10 20:08:30 +0900

    3.1.ENUM을 활용한 전략패턴

--- src/main/java/calculator/operator/OperatorMapping.java
+++ src/main/java/calculator/operator/OperatorMapping.java
@@ -4,17 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class OperatorMapping {
-    private static Map<Character, OperatorStrategy> operatorMap = new HashMap<>();
+
+    //private static Map<Character, OperatorStrategy> operatorMap = new HashMap<>();
+    private static Map<Character, OperatorType> operatorMap = new HashMap<>();
 
     static {
-        operatorMap.put('+', new AddOperator());
-        operatorMap.put('-', new SubtractOperator());
-        operatorMap.put('/', new DivideOperator());
-        operatorMap.put('*', new MultiplyOperator());
-        operatorMap.put('%', new ModOperator());
+//        operatorMap.put('+', new AddOperator());
+//        operatorMap.put('-', new SubtractOperator());
+//        operatorMap.put('/', new DivideOperator());
+//        operatorMap.put('*', new MultiplyOperator());
+//        operatorMap.put('%', new ModOperator());
+        operatorMap.put('+', OperatorType.ADD);
+        operatorMap.put('-', OperatorType.SUBTRACT);
+        operatorMap.put('*', OperatorType.MULTIPLY);
+        operatorMap.put('/', OperatorType.DIVIDE);
+        operatorMap.put('%', OperatorType.MOD);
     }
 
-    public static OperatorStrategy getOperator(char operator) {
+    public static OperatorType getOperator(char operator) {
         return operatorMap.get(operator);
     }
+//    public static OperatorStrategy getOperator(char operator) {
+//        return operatorMap.get(operator);
+//    }
 }
---
+++ src/main/java/calculator/operator/OperatorType.java
@@ -0,0 +1,39 @@
+package calculator.operator;
+
+public enum OperatorType {
+
+    ADD{
+        @Override
+        public double operate(double num1, double num2) {
+            return num1 + num2;
+        }
+    },
+    SUBTRACT{
+        @Override
+        public double operate(double num1, double num2) {
+            return num1 - num2;
+        }
+    },
+    DIVIDE{
+        @Override
+        public double operate(double num1, double num2) {
+            if (num2 == 0) throw new ArithmeticException();
+            return num1 / num2;
+        }
+    },
+    MULTIPLY{
+        @Override
+        public double operate(double num1, double num2) {
+            return num1 * num2;
+        }
+    },
+    MOD{
+        @Override
+        public double operate(double num1, double num2) {
+            return num1 % num2;
+        }
+    };
+
+    public abstract double operate(double num1, double num2); //각 enum이 가질 operate 함수
+
+}
