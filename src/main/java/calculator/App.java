305f28753041c8af54d0621d2be910cbd2ebcb4f
pigglehyun <sarah0661@pusan.ac.kr>
2025-07-10 13:32:18 +0900

    2.3.캡슐화 + 리팩토링

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import javax.swing.plaf.IconUIResource;
 import java.util.*;
 
 public class App {
@@ -8,43 +9,53 @@ public class App {
         //int index = 0;
         //int[] list = new int[10];
         Queue<Integer> list = new LinkedList<>();
+        int result = 0;
         while (go) {
-            Scanner sc = new Scanner(System.in);
-            //1.1
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int firstNum;
-            firstNum = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int secondNum;
-            secondNum = sc.nextInt();
-            //1.2
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operation;
-            operation = sc.next().charAt(0);
-            //2.2
-            Calculator calculator = new Calculator();
-            int result = 0;
-            result = calculator.calculate(firstNum, secondNum, operation);
-            System.out.println("결과: " + result);
-            //1.7
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            if ((sc.next()).equals("remove")) {
-                list.poll();
-            }
-            list.add(result);
-            //1.8
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            if ((sc.next()).equals("inquiry")) {
-                for (int l : list) {
-                    System.out.print(l + " ");
+            try {
+                Scanner sc = new Scanner(System.in);
+                //1.1
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int firstNum = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int secondNum = sc.nextInt();
+                //1.2
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operation;
+                operation = sc.next().charAt(0);
+                //2.2
+                Calculator calculator = new Calculator();
+                calculator.calculate(firstNum, secondNum, operation);
+                //2.3
+                result = calculator.getResult();
+                System.out.println("결과: " + result);
+
+                //1.7
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                if ((sc.next()).equals("remove")) {
+                    list.poll();
                 }
-                System.out.println();
+                list.add(result);
+                //1.8
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                if ((sc.next()).equals("inquiry")) {
+                    for (int l : list) {
+                        System.out.print(l + " ");
+                    }
+                    System.out.println();
+                }
+                //1.4
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                if ((sc.next()).equals("exit")) {
+                    go = false;
+                }
+            } catch (ArithmeticException e) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("연산자는 +, -, *, / 로 입력하세요.");
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력하세요.");
             }
-            //1.4
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            if ((sc.next()).equals("exit")) {
-                go = false;
-            }
+
         }
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,31 +3,31 @@ package calculator;
 public class Calculator {
 
     //2.1
-    public int result;
+    private int result;
 
-    public int calculate(int num1, int num2, char operation) {
-        try {
-            if (operation != '+' || operation != '-' || operation != '*' || operation != '/') {
-                throw new IllegalArgumentException();
-            }
-            switch (operation) {
-                case '+' -> result = num1 + num2;
-                case '-' -> result = num1 - num2;
-                case '*' -> result = num1 * num2;
-                case '/' -> {
-                    if (num2 == 0) {
-                        throw new ArithmeticException();
-                    }
-                    result = num1 / num2;
-                }
-            }
-
-        } catch (ArithmeticException e) {
-            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-        } catch (IllegalArgumentException e) {
-            System.out.println("연산자는 +, -, *, / 로 입력하세요.");
+    public void calculate(int num1, int num2, char operation) {
+        if (operation != '+' && operation != '-' && operation != '*' && operation != '/') {
+            throw new IllegalArgumentException();
         }
+        switch (operation) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if (num2 == 0) {
+                    throw new ArithmeticException();
+                }
+                result = num1 / num2;
+            }
+        }
+    }
+
+    //2.3
+    public int getResult() {
         return result;
     }
 
+    public void setResult(int result) {
+        this.result = result;
+    }
 }
