10dc4506c81e742efb9d94fc0a68fd3c878fd7a7
pigglehyun <sarah0661@pusan.ac.kr>
2025-07-10 13:18:04 +0900

    2.1.calculate함수 생성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,9 +3,13 @@ package calculator;
 public class Calculator {
 
     //2.1
-    int result ;
+    public int result;
+
     public int calculate(int num1, int num2, char operation) {
         try {
+            if (operation != '+' || operation != '-' || operation != '*' || operation != '/') {
+                throw new IllegalArgumentException();
+            }
             switch (operation) {
                 case '+' -> result = num1 + num2;
                 case '-' -> result = num1 - num2;
@@ -17,10 +21,11 @@ public class Calculator {
                     result = num1 / num2;
                 }
             }
+
         } catch (ArithmeticException e) {
             System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-        } catch ( IllegalArgumentException e) {
-            System.out.println("연산자는 더하기, 빼기, 곱하기, 나눗셈 외 입력될 수 없습니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("연산자는 +, -, *, / 로 입력하세요.");
         }
         return result;
     }
