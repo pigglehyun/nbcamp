74fbf9c5f6b6c2b2b4cff78f2972acff3d76f616
Robbie <robbie331@>
2025-07-10 10:47:55 +0900

    1.1.양의 정수 2개 입력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,16 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        int firstNum ;
+        firstNum = sc.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        int secondNum ;
+        secondNum = sc.nextInt();
     }
 }
