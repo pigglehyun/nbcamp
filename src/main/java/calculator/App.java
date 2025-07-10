6c3caa090735234a3f60792e74ad985019869b88
Robbie <robbie331@>
2025-07-10 11:03:41 +0900

    1.3.결과값 출력과 예외처리

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,13 +4,38 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try{
+            Scanner sc = new Scanner(System.in);
+            //1.1
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int firstNum ;
+            firstNum = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int secondNum ;
+            secondNum = sc.nextInt();
+            //1.2
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operation;
+            operation = sc.next().charAt(0);
 
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        int firstNum ;
-        firstNum = sc.nextInt();
-        System.out.println("두 번째 숫자를 입력하세요: ");
-        int secondNum ;
-        secondNum = sc.nextInt();
+            int result = 0;
+            switch (operation) {
+                case '+' -> result = firstNum + secondNum;
+                case '-' -> result = firstNum - secondNum;
+                case '*' -> result = firstNum * secondNum;
+                case '/' -> {
+                    if (secondNum == 0) {
+                        throw new ArithmeticException();
+                    }
+                    result = firstNum / secondNum;
+                }
+            }
+
+            System.out.println("결과: " + result);
+        } catch ( ArithmeticException e) {
+            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+        }
+
+
     }
 }
