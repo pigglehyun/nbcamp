863ea3804d8bdc83a77072bb156e93e61a5119b1
Robbie <robbie331@>
2025-07-10 11:12:29 +0900

    1.4.반복문으로 계산 반복

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,37 +5,44 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         try{
-            Scanner sc = new Scanner(System.in);
-            //1.1
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int firstNum ;
-            firstNum = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int secondNum ;
-            secondNum = sc.nextInt();
-            //1.2
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operation;
-            operation = sc.next().charAt(0);
+            boolean go = true ;
+            while ( go ) {
+                Scanner sc = new Scanner(System.in);
+                //1.1
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int firstNum ;
+                firstNum = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int secondNum ;
+                secondNum = sc.nextInt();
+                //1.2
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operation;
+                operation = sc.next().charAt(0);
 
-            int result = 0;
-            switch (operation) {
-                case '+' -> result = firstNum + secondNum;
-                case '-' -> result = firstNum - secondNum;
-                case '*' -> result = firstNum * secondNum;
-                case '/' -> {
-                    if (secondNum == 0) {
-                        throw new ArithmeticException();
+                int result = 0;
+                switch (operation) {
+                    case '+' -> result = firstNum + secondNum;
+                    case '-' -> result = firstNum - secondNum;
+                    case '*' -> result = firstNum * secondNum;
+                    case '/' -> {
+                        if (secondNum == 0) {
+                            throw new ArithmeticException();
+                        }
+                        result = firstNum / secondNum;
                     }
-                    result = firstNum / secondNum;
+                }
+                System.out.println("결과: " + result);
+
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                if ( (sc.next()).equals("exit") ) {
+                    go = false;
                 }
             }
 
-            System.out.println("결과: " + result);
+
         } catch ( ArithmeticException e) {
             System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
         }
-
-
     }
 }
